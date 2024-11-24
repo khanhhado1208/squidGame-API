c6b89dbd40c3ff2694dafe83bb9b9d27aad19e69
H D <ha.k.do@aalto.fi>
2024-11-24 22:52:18 +0200

    Fix quote

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ const app = express();
 app.use(cors());
 
 const quotes = [
-    { id: 1, text: "You Took More From Me Than Whatever I Might Possibly Owe.", character: "Kang Sae-Byeok" },
-    { id: 2, text: "Do You Know What Someone With No Money Has In Common With Someone With Too Much Money? Living Is No Fun For Them", character: "Oh Il-nam" },
-    { id: 3, text: "This Is Hell. What Are The Rules In Hell", character: "Jang Deok-Su" },
-    { id: 4, text: "I Wanted To Just Feel Something, Just One Last Time Before I Die", character: "Oh Il-Nam" },
-    { id: 5, text: "We’ve Already Come Too Far To End This Now.", character: "Cho Sang-Woo" },
-    { id: 6, text: "Don’t Do It. That Isn’t You. You’re A Good Person At Heart.", character: "Kang Sae-Byeok" },
-    { id: 7, text: "I Don’t Have A Home To Go Back To. In Here, I Stand A Chance At Least. But Out There? I Got Nothing Out There", character: "Player 322" },
-    { id: 8, text: "“Just In Case Either Of Us Can Actually Make It Out Of This Hellhole Somehow, We’ll Look After Each Other’s Loved Ones, Okay?", character: "Kang Sae-Byeok" },
-    { id: 9, text: "Mom, Are You Asleep? Hey, Ma. Mom, I’m Home. Come On. I Came Home, Ma. Just, Just Open Your Eyes. Come On.", character: "Seong Gi-Hun" },
-    { id: 10, text: "You Have A Reason To Leave This Place, But I Don’t.", character: "Ji-Yeong" },
-    { id: 11, text: "Don’t Do It. That Isn’t You. You’re A Good Person At Heart.", character: "Kang Sae-Byeok" },
-    { id: 12, text: "You can’t turn back now. There is no turning back from this point", character: "Player 456" },
-    { id: 13, text: "Stick with the strongest team if you want to survive.", character: "Heo Sung-tae" },
-    { id: 14, text: "Doing something is always more fun than just watching", character: " Kan Sae-byeok" }
-];
-
+    { id: 1, text: "You took more from me than whatever I might possibly owe.", character: "Kang Sae-Byeok" },
+    { id: 2, text: "Do you know what someone with no money has in common with someone with too much money? Living is no fun for them.", character: "Oh Il-nam" },
+    { id: 3, text: "This is hell. What are the rules in hell?", character: "Jang Deok-Su" },
+    { id: 4, text: "I wanted to just feel something, just one last time before I die.", character: "Oh Il-nam" },
+    { id: 5, text: "We’ve already come too far to end this now.", character: "Cho Sang-Woo" },
+    { id: 6, text: "Don’t do it. That isn’t you. You’re a good person at heart.", character: "Kang Sae-Byeok" },
+    { id: 7, text: "I don’t have a home to go back to. In here, I stand a chance at least. But out there? I’ve got nothing out there.", character: "Player 322" },
+    { id: 8, text: "Just in case either of us can actually make it out of this hellhole somehow, we’ll look after each other’s loved ones, okay?", character: "Kang Sae-Byeok" },
+    { id: 9, text: "Mom, are you asleep? Hey, Ma. Mom, I’m home. Come on. I came home, Ma. Just, just open your eyes. Come on.", character: "Seong Gi-Hun" },
+    { id: 10, text: "You have a reason to leave this place, but I don’t.", character: "Ji-Yeong" },
+    { id: 11, text: "You can’t turn back now. There is no turning back from this point.", character: "Player 456" },
+    { id: 12, text: "Stick with the strongest team if you want to survive.", character: "Heo Sung-tae" },
+    { id: 13, text: "Doing something is always more fun than just watching.", character: "Kang Sae-Byeok" },
+    { id: 14, text: "The world’s most dangerous people are the ones who have nothing to lose.", character: "Oh Il-nam" }
+  ];
+  
+  
 app.get('/api/:id', (req, res)=>{
     const id = parseInt(req.params.id); //  Get Id from URL
     const quote = quotes.find(q=>q.id === id); // find correspoding id
